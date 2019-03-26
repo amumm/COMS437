7423e2053ca54e5d99af4cb24c676bf2f10f5df0
MummA2-PC\MummA2 <mumm98@gmail.com>
2019-03-25 22:07:20 -0500

    Needed to manually add BEPU Physics to the project in order to get the conversion helper

---
+++ Assignments/Assignment 1B/Asteroid/Asteroid/Asteroids.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using BEPUphysics;
+
+namespace Asteroid
+{
+    internal class Asteroids : DrawableGameComponent
+    {
+
+        private Model model;
+        private Texture2D moonTexture;
+        private BEPUphysics.Entities.Prefabs.Sphere physicsObject;
+        private Vector3 CurrentPosition
+        {
+            get
+            {
+                return ConversionHelper.MathConverter.Convert(physicsObject.Position);
+            }
+        }
+
+        public Asteroids(Game game) : base(game)
+        {
+            game.Components.Add(this);
+        }
+
+        public Asteroids(Game game, Vector3 pos) : this(game)
+        {
+            physicsObject = new BEPUphysics.Entities.Prefabs.Sphere(ConversionHelper.MathConverter.Convert(pos), 1)
+            {
+                AngularDamping = 0f,
+                LinearDamping = 0f
+            };
+            Game.Services.GetService<Space>().Add(physicsObject);
+        }
+
+        public Asteroids(Game game, Vector3 pos, float mass) : this(game, pos)
+        {
+            physicsObject.Mass = mass;
+        }
+
+        public Asteroids(Game game, Vector3 pos, float mass, Vector3 linMomentum) : this(game, pos, mass)
+        {
+            physicsObject.LinearMomentum = ConversionHelper.MathConverter.Convert(linMomentum);
+        }
+
+        public Asteroids(Game game, Vector3 pos, float mass, Vector3 linMomentum, Vector3 angMomentum) : this(game, pos, mass, linMomentum)
+        {
+            physicsObject.AngularMomentum = ConversionHelper.MathConverter.Convert(angMomentum);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            moonTexture = Game.Content.Load<Texture2D>("moonsurface");
+            model = Game.Content.Load<Model>("moon");
+            physicsObject.Radius = model.Meshes[0].BoundingSphere.Radius;
+
+
+            base.LoadContent();
+        }
+
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            foreach (var mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.DirectionalLight0.DiffuseColor = new Vector3(10, 10, 10);
+                    effect.DirectionalLight1.DiffuseColor = new Vector3(10, 10, 10);
+                    effect.DirectionalLight2.DiffuseColor = new Vector3(10, 10, 10);
+                    effect.PreferPerPixelLighting = true;
+                    effect.World = ConversionHelper.MathConverter.Convert(physicsObject.WorldTransform);
+                    effect.View = Matrix.CreateLookAt(Main.CameraPosition, Main.CameraDirection, Vector3.Up);
+                    float aspectRatio = Game.GraphicsDevice.Viewport.AspectRatio;
+                    float fieldOfView = Microsoft.Xna.Framework.MathHelper.PiOver4;
+                    float nearClipPlane = 0.3f;
+                    float farClipPlane = 200f;
+                    effect.Projection = Matrix.CreatePerspectiveFieldOfView(fieldOfView, aspectRatio, nearClipPlane, farClipPlane);
+                }
+                mesh.Draw();
+            }
+            base.Draw(gameTime);
+        }
+
+    }
+}
--- Assignments/Assignment 1B/Asteroid/Asteroid/Game1.cs
+++ Assignments/Assignment 1B/Asteroid/Asteroid/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using BEPUphysics;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -11,19 +12,45 @@ namespace Asteroid
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+        // The star background of the game
         Model skybox;
+
+
         Vector3 modelPosition;
-        Vector3 cameraPosition;
+
+        // The location of the camera in the world
+        public static Vector3 CameraPosition
+        {
+            get;
+            private set;
+        }
+
+        // The direction the camera is looking in the world
+        public static Vector3 CameraDirection
+        {
+            get;
+            private set;
+        }
+
+
         float rotation;
 
+        // The height of the screen
         float windowHeight;
+
+        // The width of the screen
         float windowWidth;
+
+        // The aspect ratio for the screen
         float aspectRatio;
 
         public Main()
         {
-            graphics = new GraphicsDeviceManager(this);
-            graphics.GraphicsProfile = GraphicsProfile.HiDef;
+            graphics = new GraphicsDeviceManager(this)
+            {
+                GraphicsProfile = GraphicsProfile.HiDef
+            };
 
             Content.RootDirectory = "Content";
         }
@@ -36,8 +63,22 @@ namespace Asteroid
         /// </summary>
         protected override void Initialize()
         {
+
+            // Make our BEPU Physics space a service
+            Services.AddService<Space>(new Space());
+
+            // Create two asteroids.  Note that asteroids automatically add themselves to
+            // as a DrawableGameComponent as well as add an object into Bepu physics
+            // that represents the asteroid.
+
+            new Asteroids(this, new Vector3(-2, 0, -5), 2, new Vector3(0.2f, 0, 0), new Vector3(0.3f, 0.5f, 0.5f));
+            new Asteroids(this, new Vector3(2, 0, -5), 3, new Vector3(-0.2f, 0, 0), new Vector3(-0.5f, -0.6f, 0.2f));
+
             modelPosition = Vector3.Zero;
-            cameraPosition = new Vector3(0, 0, 0.1f);
+
+            CameraPosition = new Vector3(0, 0, 0.1f);
+            CameraDirection = new Vector3(0, 0, -1);
+
             windowHeight = graphics.PreferredBackBufferHeight;
             windowWidth = graphics.PreferredBackBufferWidth;
             aspectRatio = windowWidth / windowHeight;
@@ -80,6 +121,9 @@ namespace Asteroid
 
             rotation += 0.005f;
 
+            // Update the physics engine based on how many seconds have passed since last update.
+            Services.GetService<Space>().Update((float)gameTime.ElapsedGameTime.TotalSeconds);
+
             base.Update(gameTime);
         }
 
@@ -92,7 +136,7 @@ namespace Asteroid
             GraphicsDevice.Clear(Color.Yellow);
 
             Matrix world = Matrix.CreateScale(1.0f) * Matrix.CreateRotationX(-MathHelper.PiOver2) * Matrix.CreateRotationY(rotation) * Matrix.CreateTranslation(modelPosition);
-            Matrix view = Matrix.CreateLookAt(cameraPosition, modelPosition, Vector3.UnitY);
+            Matrix view = Matrix.CreateLookAt(CameraPosition, modelPosition, Vector3.UnitY);
             Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), aspectRatio, 0.01f, 1000f);
             foreach (ModelMesh mesh in skybox.Meshes)
             {
