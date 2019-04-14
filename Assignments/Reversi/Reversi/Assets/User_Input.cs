d25e63b096d25b28e1a66c15664252c4488a1089
MummA2-PC\MummA2 <mumm98@gmail.com>
2019-04-13 22:09:26 -0500

    Adding logic to only place a piece if you are allowed

--- Assignments/Reversi/Reversi/Assets/User_Input.cs
+++ Assignments/Reversi/Reversi/Assets/User_Input.cs
@@ -9,7 +9,7 @@ public class User_Input : MonoBehaviour
 
     private const int numberOfRows = 8;
     private const int numberOfColumns = 8;
-    private const float baseTileHeight = 1.1f;
+    private const float baseTileHeight = 1.5f;
     private Vector3 blackOrientation = new Vector3(0, 0, 180);
     private Vector3 whiteOrientation = new Vector3(0, 0, 0);
     private GameObject[,] pieces;
@@ -51,9 +51,55 @@ public class User_Input : MonoBehaviour
         int col = (int)(point.x / 1.0f);
 
         Debug.Log("Row: " + row + " Col: " + col);
-        if (row >= 0 && row < 8 && col >= 0 && col < 8 && pieces[row, col] == null)
+        if (isInBounds(row, col) && pieces[row, col] == null)
         {
-            pieces[row, col] = Instantiate(piece, new Vector3(col + 0.5f, baseTileHeight, -row - 0.5f), Quaternion.Euler(blackOrientation));
+            if (canPlacePiece(row, col))
+                pieces[row, col] = Instantiate(piece, new Vector3(col + 0.5f, baseTileHeight, -row - 0.5f), Quaternion.Euler(blackOrientation));
         }
     }
+
+    bool isInBounds(int row, int col)
+    {
+        return row >= 0 && row < 8 && col >= 0 && col < 8;
+    }
+
+    bool canPlacePiece(int row, int col)
+    {
+        bool flipTopLeft = checkDirection(row, col, -1, 1);
+        bool flipTop = checkDirection(row, col, 0, 1);
+        bool flipTopRight = checkDirection(row, col, 1, 1);
+        bool flipLeft = checkDirection(row, col, -1, 0);
+        bool flipRight = checkDirection(row, col, 1, 0);
+        bool flipBottomLeft = checkDirection(row, col, -1, -1);
+        bool flipBottom = checkDirection(row, col, 0, -1);
+        bool flipBottomRight = checkDirection(row, col, 1, -1);
+
+        return flipTopLeft || flipTop || flipTopRight || flipLeft || flipRight || flipBottomLeft || flipBottom || flipBottomRight;
+    }
+
+    bool checkDirection(int row, int col, int x, int z)
+    {
+        if (!isInBounds(row + z, col + x))
+            return false;
+
+        GameObject cur = pieces[row + z, col + x];
+        if (cur == null)
+            return false;
+
+        bool foundOppositeColor = false;
+        var rotation = cur.transform.rotation;
+        while(rotation.z == 0)
+        {
+            foundOppositeColor = true;
+            if (isInBounds(row + z, col + x))
+                cur = pieces[row + z, col + x];
+            else
+                return false;
+
+            if (cur == null)
+                return false;
+        }
+        return foundOppositeColor;
+    }
+
 }
