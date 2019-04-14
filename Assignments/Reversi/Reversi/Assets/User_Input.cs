a56a29d267b3080acd2c706733577a4338c321cc
MummA2-PC\MummA2 <mumm98@gmail.com>
2019-04-13 20:55:53 -0500

    Can click to add a new tile

--- Assignments/Reversi/Reversi/Assets/User_Input.cs
+++ Assignments/Reversi/Reversi/Assets/User_Input.cs
@@ -10,18 +10,21 @@ public class User_Input : MonoBehaviour
     private const int numberOfRows = 8;
     private const int numberOfColumns = 8;
     private const float baseTileHeight = 1.1f;
+    private Vector3 blackOrientation = new Vector3(0, 0, 180);
+    private Vector3 whiteOrientation = new Vector3(0, 0, 0);
     private GameObject[,] pieces;
+
+    private bool playersTurn = true;
 
     // Start is called before the first frame update
     void Start()
     {
         pieces = new GameObject[numberOfRows, numberOfColumns];
 
-        pieces[3, 3] = Instantiate(piece, new Vector3(-0.5f, baseTileHeight, 0.5f), Quaternion.identity);
-        pieces[3, 4] = Instantiate(piece, new Vector3(0.5f, baseTileHeight, 0.5f), Quaternion.identity);
-        pieces[3, 3] = Instantiate(piece, new Vector3(-0.5f, baseTileHeight, -0.5f), Quaternion.identity);
-        pieces[4, 4] = Instantiate(piece, new Vector3(0.5f, baseTileHeight, -0.5f), Quaternion.identity);
-
+        pieces[3, 3] = Instantiate(piece, new Vector3(3.5f, baseTileHeight, -3.5f), Quaternion.Euler(blackOrientation));
+        pieces[3, 4] = Instantiate(piece, new Vector3(4.5f, baseTileHeight, -3.5f), Quaternion.Euler(whiteOrientation));
+        pieces[4, 3] = Instantiate(piece, new Vector3(3.5f, baseTileHeight, -4.5f), Quaternion.Euler(whiteOrientation));
+        pieces[4, 4] = Instantiate(piece, new Vector3(4.5f, baseTileHeight, -4.5f), Quaternion.Euler(blackOrientation));
     }
 
     // Update is called once per frame
@@ -34,12 +37,23 @@ public class User_Input : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
-                Debug.Log(hit.transform.name);
-                if (hit.transform.name == "Model_Board")
+                if (hit.transform.name == "Board")
                 {
-                    Debug.Log(hit.point);
+                    getCell(hit.point);
                 }
             }
+        }
+    }
+
+    void getCell(Vector3 point)
+    {
+        int row = (int)(point.z / -1.0f);
+        int col = (int)(point.x / 1.0f);
+
+        Debug.Log("Row: " + row + " Col: " + col);
+        if (row >= 0 && row < 8 && col >= 0 && col < 8 && pieces[row, col] == null)
+        {
+            pieces[row, col] = Instantiate(piece, new Vector3(col + 0.5f, baseTileHeight, -row - 0.5f), Quaternion.Euler(blackOrientation));
         }
     }
 }
