b1f017e89fedeedbe835eab8bbf47953685e0036
amumm <amumm@iastate.edu>
2019-04-27 22:29:12 -0500

    Set up the rest of the game UI

--- Assignments/Reversi/Reversi/Assets/User_Input.cs
+++ Assignments/Reversi/Reversi/Assets/User_Input.cs
@@ -21,6 +21,12 @@ public class User_Input : MonoBehaviour
     public GameObject scoreTextBlack;
     public GameObject scoreTextWhite;
 
+    public GameObject mainPanel;
+
+    public GameObject nextTurnPanel;
+    public GameObject nextTurnText;
+    public Button nextTurnButton;
+
     private int difficulty;
     public Button difficultyButton1;
     public Button difficultyButton2;
@@ -29,11 +35,14 @@ public class User_Input : MonoBehaviour
     public Button difficultyButton5;
     public GameObject difficultyPanel;
 
+    public GameObject startNewGamePanel;
+    public GameObject startNewGameText;
+    public Button startNewGameButton;
+
     private float waitTime = 2f;
     private float timeBetween = 0f;
 
     private bool playersTurn = true;
-
     private bool computerCanMove = true;
     private bool humanCanMove = true;
     private bool gameOver = false;
@@ -43,24 +52,17 @@ public class User_Input : MonoBehaviour
     {
         pieces = new PieceNode[numberOfRows, numberOfColumns];
         
-        var gameObject1 = Instantiate(blackPiece, new Vector3(3.5f, baseTileHeight, -3.5f), Quaternion.Euler(blackOrientation));
-        var gameObject2 = Instantiate(whitePiece, new Vector3(4.5f, baseTileHeight, -3.5f), Quaternion.Euler(whiteOrientation));
-        var gameObject3 = Instantiate(whitePiece, new Vector3(3.5f, baseTileHeight, -4.5f), Quaternion.Euler(whiteOrientation));
-        var gameObject4 = Instantiate(blackPiece, new Vector3(4.5f, baseTileHeight, -4.5f), Quaternion.Euler(blackOrientation));
-
-        pieces[3, 3] = new PieceNode(Player.black, 3, 3, gameObject1);
-        pieces[3, 4] = new PieceNode(Player.white, 3, 4, gameObject2);
-        pieces[4, 3] = new PieceNode(Player.white, 4, 3, gameObject3);
-        pieces[4, 4] = new PieceNode(Player.black, 4, 4, gameObject4);
-
-        scoreTextBlack.GetComponent<Text>().text = "# Black: 2";
-        scoreTextWhite.GetComponent<Text>().text = "# White: 2";
-
         difficultyButton1.onClick.AddListener(delegate { setDifficulty(1); });
         difficultyButton2.onClick.AddListener(delegate { setDifficulty(2); });
         difficultyButton3.onClick.AddListener(delegate { setDifficulty(3); });
         difficultyButton4.onClick.AddListener(delegate { setDifficulty(4); });
         difficultyButton5.onClick.AddListener(delegate { setDifficulty(5); });
+
+        nextTurnButton.onClick.AddListener(delegate { switchTurns(); });
+
+        startNewGameButton.onClick.AddListener(delegate { startNewGame(); });
+
+        startNewGame();
     }
 
     // Update is called once per frame
@@ -70,6 +72,8 @@ public class User_Input : MonoBehaviour
         if (!computerCanMove && !humanCanMove && !gameOver)
         {
             Debug.Log("Game Over");
+            mainPanel.SetActive(true);
+            startNewGamePanel.SetActive(true);
             gameOver = true;
             setScore();
             return;
@@ -105,8 +109,10 @@ public class User_Input : MonoBehaviour
             } else {
                 if (humanHasMoves())
                     Debug.Log("Computer cannot make a move");
+                nextTurnText.GetComponent<Text>().text = "Computer has no available moves. It is now your turn.";
+                nextTurnPanel.SetActive(true);
+                mainPanel.SetActive(true);
                 computerCanMove = false;
-                playersTurn = !playersTurn;
             }
 
             timeBetween = 0f;
@@ -117,6 +123,49 @@ public class User_Input : MonoBehaviour
     {
         this.difficulty = difficulty;
         difficultyPanel.SetActive(false);
+        mainPanel.SetActive(false);
+    }
+
+    private void switchTurns()
+    {
+        playersTurn = !playersTurn;
+        nextTurnPanel.SetActive(false);
+        mainPanel.SetActive(false);
+    }
+
+    private void startNewGame()
+    {
+        foreach (PieceNode piece in pieces)
+        {
+            if (piece != null)
+            {
+                if (piece.gameObject != null)
+                    Destroy(piece.gameObject);
+            }
+        }
+        Array.Clear(pieces, 0, pieces.Length);
+
+        var gameObject1 = Instantiate(blackPiece, new Vector3(3.5f, baseTileHeight, -3.5f), Quaternion.Euler(blackOrientation));
+        var gameObject2 = Instantiate(whitePiece, new Vector3(4.5f, baseTileHeight, -3.5f), Quaternion.Euler(whiteOrientation));
+        var gameObject3 = Instantiate(whitePiece, new Vector3(3.5f, baseTileHeight, -4.5f), Quaternion.Euler(whiteOrientation));
+        var gameObject4 = Instantiate(blackPiece, new Vector3(4.5f, baseTileHeight, -4.5f), Quaternion.Euler(blackOrientation));
+
+        pieces[3, 3] = new PieceNode(Player.black, 3, 3, gameObject1);
+        pieces[3, 4] = new PieceNode(Player.white, 3, 4, gameObject2);
+        pieces[4, 3] = new PieceNode(Player.white, 4, 3, gameObject3);
+        pieces[4, 4] = new PieceNode(Player.black, 4, 4, gameObject4);
+
+        scoreTextBlack.GetComponent<Text>().text = "# Black: 2";
+        scoreTextWhite.GetComponent<Text>().text = "# White: 2";
+
+        nextTurnPanel.SetActive(false);
+        startNewGamePanel.SetActive(false);
+        difficultyPanel.SetActive(true);
+
+        playersTurn = true;
+        humanCanMove = true;
+        computerCanMove = true;
+        gameOver = false;
     }
 
     void setScore()
@@ -138,6 +187,12 @@ public class User_Input : MonoBehaviour
         if (gameOver)
         {
             Debug.Log("# Black: " + numBlack + " # White: " + numWhite);
+            if (numBlack > numWhite)
+                startNewGameText.GetComponent<Text>().text = "You Win!";
+            else if (numBlack < numWhite)
+                startNewGameText.GetComponent<Text>().text = "You Lose!";
+            else
+                startNewGameText.GetComponent<Text>().text = "Tie Game";
         }
     }
 
@@ -159,9 +214,12 @@ public class User_Input : MonoBehaviour
         if (moves.Count == 0)
         {
             if (computerCanMove && playersTurn)
+            {
+                nextTurnText.GetComponent<Text>().text = "You have no available moves. It is the computers turn.";
+                mainPanel.SetActive(true);
+                nextTurnPanel.SetActive(true);
                 Debug.Log("You cannot make a move");
-            if (playersTurn)
-                playersTurn = false;
+            }
             humanCanMove = false;
             return false;
         }
