32874410cd9deb6c66675121a295638b2bafcfb2
MummA2-PC\MummA2 <mumm98@gmail.com>
2019-05-08 20:54:42 -0500

    Added the player and movements

---
+++ Assignments/CyBot/CyBots/Assets/Movement.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 6.0F;
+    public float rotationSpeed = 5.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    public CharacterController controller;
+
+    private Quaternion target;
+
+    void Start()
+    {
+        Quaternion target = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+    }
+
+    void Update()
+    {
+        var forward = Input.GetKey("w");
+        var backward = Input.GetKey("s");
+        var turnRight = Input.GetKey("a");
+        var turnLeft = Input.GetKey("d");
+
+        if (controller.isGrounded)
+        {
+            moveDirection.x *= 0.8f;
+            moveDirection.z *= 0.8f;
+
+            if (forward)
+            {
+                moveDirection = transform.forward * moveSpeed;
+            }
+            if (backward)
+            {
+                moveDirection = transform.forward * -moveSpeed;
+            }
+
+            if (Input.GetKey("space"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+
+        }
+        else {
+            moveDirection.x *= 0.96f;
+            moveDirection.z *= 0.96f;
+        }
+
+        if (turnRight)
+        {
+            transform.Rotate(new Vector3(0, transform.rotation.y - rotationSpeed, 0), 100.0f * Time.deltaTime);
+        }
+
+        if (turnLeft)
+        {
+            transform.Rotate(new Vector3(0, transform.rotation.y + rotationSpeed, 0), 100.0f * Time.deltaTime);
+        }
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 5.0f);
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+}
